67529208d1c7460626e7c4d638a9a9bbb2bb6ddd
ISHII 2bit <ishii@buffer-renaiss.com>
2013-08-31 15:49:59 +0900

    add voice names constants

---
+++ src/ofxVoices.cpp
@@ -0,0 +1,52 @@
+//
+//  ofxVoices.cpp
+//
+//  Created by ISHII 2bit on 2013/08/31.
+//
+//
+
+#include "ofxVoices.h"
+
+using namespace std;
+
+namespace ofxVoices {
+    const string appleSpeechVoiceDomain = "com.apple.speech.synthesis.voice.";
+    
+    const string Agnes = appleSpeechVoiceDomain + "Agnes";
+    const string Albert = appleSpeechVoiceDomain + "Albert";
+    const string Alex = appleSpeechVoiceDomain + "Alex";
+    const string BadNews = appleSpeechVoiceDomain + "BadNews";
+    const string Bahh = appleSpeechVoiceDomain + "Bahh";
+    const string Bells = appleSpeechVoiceDomain + "Bells";
+    const string Boing = appleSpeechVoiceDomain + "Boing";
+    const string Bruce = appleSpeechVoiceDomain + "Bruce";
+    const string Bubbles = appleSpeechVoiceDomain + "Bubbles";
+    const string Cellos = appleSpeechVoiceDomain + "Cellos";
+    const string daniel = appleSpeechVoiceDomain + "daniel.premium";
+    const string Deranged = appleSpeechVoiceDomain + "Deranged";
+    const string emily = appleSpeechVoiceDomain + "emily.premium";
+    const string fiona = appleSpeechVoiceDomain + "fiona.premium";
+    const string Fred = appleSpeechVoiceDomain + "Fred";
+    const string GoodNews = appleSpeechVoiceDomain + "GoodNews";
+    const string Hysterical = appleSpeechVoiceDomain + "Hysterical";
+    const string jill = appleSpeechVoiceDomain + "jill.premium";
+    const string Junior = appleSpeechVoiceDomain + "Junior";
+    const string karen = appleSpeechVoiceDomain + "karen.premium";
+    const string Kathy = appleSpeechVoiceDomain + "Kathy";
+    const string kyoko = appleSpeechVoiceDomain + "kyoko.premium";
+    const string lee = appleSpeechVoiceDomain + "lee.premium";
+    const string moira = appleSpeechVoiceDomain + "moira.premium";
+    const string Organ = appleSpeechVoiceDomain + "Organ";
+    const string Princess = appleSpeechVoiceDomain + "Princess";
+    const string Ralph = appleSpeechVoiceDomain + "Ralph";
+    const string samantha = appleSpeechVoiceDomain + "samantha.premium";
+    const string sangeeta = appleSpeechVoiceDomain + "sangeeta.premium";
+    const string serena = appleSpeechVoiceDomain + "serena.premium";
+    const string tessa = appleSpeechVoiceDomain + "tessa.premium";
+    const string tom = appleSpeechVoiceDomain + "tom.premium";
+    const string Trinoids = appleSpeechVoiceDomain + "Trinoids";
+    const string Vicki = appleSpeechVoiceDomain + "Vicki";
+    const string Victoria = appleSpeechVoiceDomain + "Victoria";
+    const string Whisper = appleSpeechVoiceDomain + "Whisper";
+    const string Zarvox = appleSpeechVoiceDomain + "Zarvox";
+};
---
+++ src/ofxVoices.h
@@ -0,0 +1,55 @@
+//
+//  ofxVoices.h
+//
+//  Created by ISHII 2bit on 2013/08/31.
+//
+//
+
+#ifndef __ofxVoiceSynthesizerExample__ofxVoices__
+#define __ofxVoiceSynthesizerExample__ofxVoices__
+
+#include <iostream>
+
+namespace ofxVoices {
+    extern const std::string appleSpeechVoiceDomain;
+    
+    extern const std::string Agnes;
+    extern const std::string Albert;
+    extern const std::string Alex;
+    extern const std::string BadNews;
+    extern const std::string Bahh;
+    extern const std::string Bells;
+    extern const std::string Boing;
+    extern const std::string Bruce;
+    extern const std::string Bubbles;
+    extern const std::string Cellos;
+    extern const std::string daniel;
+    extern const std::string Deranged;
+    extern const std::string emily;
+    extern const std::string fiona;
+    extern const std::string Fred;
+    extern const std::string GoodNews;
+    extern const std::string Hysterical;
+    extern const std::string jill;
+    extern const std::string Junior;
+    extern const std::string karen;
+    extern const std::string Kathy;
+    extern const std::string kyoko;
+    extern const std::string lee;
+    extern const std::string moira;
+    extern const std::string Organ;
+    extern const std::string Princess;
+    extern const std::string Ralph;
+    extern const std::string samantha;
+    extern const std::string sangeeta;
+    extern const std::string serena;
+    extern const std::string tessa;
+    extern const std::string tom;
+    extern const std::string Trinoids;
+    extern const std::string Vicki;
+    extern const std::string Victoria;
+    extern const std::string Whisper;
+    extern const std::string Zarvox;
+};
+
+#endif /* defined(__ofxVoiceSynthesizerExample__ofxVoices__) */
